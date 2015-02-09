f0513f62959c90f2074b2b75c79c2a2f44ef0add
Matt May <matthewbentonmay@gmail.com>
2015-02-09 10:47:40 -0600

    added dependent:destroy so when a restaurant is deleted, so is the rec

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :ratings
+  has_many :recommendations, dependent: :destroy
   has_attached_file :avatar, default_url: '/images/missing.jpg'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true
